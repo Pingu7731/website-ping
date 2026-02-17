b6355273c6869d2c1faaa62ac18859ec1bd11197
Noot <101384541+Pingu7731@users.noreply.github.com>
2026-02-17 22:48:27 +0800

    image?

--- pingweb/src/components/button.tsx
+++ pingweb/src/components/button.tsx
@@ -6,7 +6,7 @@ const Button = ({ className, id, text }: { className?: string; id?: string; text
                 <div className="bg-circle" />
                 <p className="text"> {text}</p>
                 <div className="arrow-wrapper">
-                    <img src="https://cdn.discordapp.com/attachments/1015398144135139398/1219517700657643611/pngV3_PingWatermark.png?ex=698f5f1d&is=698e0d9d&hm=70f0d0e71596ceae5065be2a8e4786f70aec4fe478ad981c4cc88c01c6140722&" alt="pic" />
+                    <img src="./src/images/icon.png" alt="pic" />
                 </div>
             </div>
         </a>
