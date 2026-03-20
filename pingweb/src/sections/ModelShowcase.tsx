e11459b42be8579a2079e8ec8c30a3dc7de36710
Ping <pingping7731@gmail.com>
2026-03-21 02:01:50 +0800

    gsap anim for model showcase

--- pingweb/src/sections/ModelShowcase.tsx
+++ pingweb/src/sections/ModelShowcase.tsx
@@ -1,21 +1,63 @@
+import { useRef } from "react";
 import { modelName } from "../constants";
-// import gsap from 'gsap'
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+
+gsap.registerPlugin(ScrollTrigger)
+
 const ModelShowcase = () => {
+    const sectionRef = useRef(null);
+    const modelRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+    useGSAP(() => {
+
+        modelRefs.current.forEach((card, index) => {
+            gsap.fromTo(
+                card,
+                {
+                    y: 50,
+                    opacity: 0
+                },
+                {
+                    y: 0,
+                    opacity: 1,
+                    duration: 0.1,
+                    delay: 0.2 * (index + 1),
+                    // trigger here
+                    scrollTrigger: {
+                        trigger: card,
+                        start: 'top bottom-=100'
+                    }
+                }
+            );
+        });
+        gsap.fromTo(
+            sectionRef.current,
+            { opacity: 0 },
+            { opacity: 1, duration: 0.5 }
+        );
+    }, []);
     return (
 
-        <div id="model" className="app-showcase">
+        <div ref={sectionRef} id="model" className="app-showcase">
             {/* <div>Me</div> */}
             <div className="w-full ">
                 <div className="showcaselayout ">
                     {/*xl:[&:has(.image-wrapper:hover)]:flex-2  --> make sure im hovering image*/}
                     {modelName.map((item, i) =>
-                        <div key={i} className="
+
+                        <div key={i}
+                            ref={(el) => {
+                                modelRefs.current[i] = el;
+                            }}
+                            className="
                         first-project-wrapper
                         flex-1
                         transition-all duration-500
                         xl:[&:has(.image-wrapper:hover)]:flex-2 
                         ">
-                            <div className="
+                            <div id={"img_" + i.toString()} className="
                             image-wrapper opacity-100 transition-all duration-300  
                             xl:opacity-50 
                             xl:hover:w-full 
