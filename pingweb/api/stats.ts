c04d98e59f03faf37de753873c2d46022875fd3e
Ping <pingping7731@gmail.com>
2026-04-06 20:09:43 +0800

    vercel

---
+++ pingweb/api/stats.ts
@@ -0,0 +1,38 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(
+    req: VercelRequest,
+    res: VercelResponse
+) {
+    const CHANNEL_ID = process.env.SE_CHANNEL_ID;
+    const JWT_TOKEN = process.env.SE_JWT_TOKEN;
+
+
+    if (!CHANNEL_ID || !JWT_TOKEN) {
+        return res.status(500).json({ error: 'Missing env' });
+    }
+
+    const url = `https://api.streamelements.com/kappa/v2/sessions/${CHANNEL_ID}`;
+
+    try {
+        const response = await fetch(url, {
+            headers: {
+                'Authorization': `Bearer ${JWT_TOKEN}`,
+                'Accept': 'application/json'
+            }
+        });
+
+        // 檢查 StreamElements API 是否有回傳成功
+        if (!response.ok) {
+            const errorText = await response.text();
+            return res.status(response.status).json({ error: 'SE API Error', details: errorText });
+        }
+
+        const data = await response.json();
+
+        return res.status(200).json(data);
+    } catch (error) {
+        console.error("Backend Error:", error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
--- pingweb/src/Api.ts
+++ pingweb/src/Api.ts
@@ -1,18 +1,33 @@
 
+// export const getMyActivities = async () => {
+//     const CHANNEL_ID = import.meta.env.VITE_SE_CHANNEL_ID;
+//     const JWT_TOKEN = import.meta.env.VITE_SE_JWT_TOKEN;
+
+//     const url = `https://api.streamelements.com/kappa/v2/sessions/${CHANNEL_ID}`;
+
+//     const response = await fetch(url, {
+//         headers: {
+//             'Authorization': `Bearer ${JWT_TOKEN}`,
+//             'Accept': 'application/json'
+//         }
+//     });
+
+//     const data = await response.json();
+//     console.log(data);
+//     return data;
+// };
+
 export const getMyActivities = async () => {
-    const CHANNEL_ID = import.meta.env.VITE_SE_CHANNEL_ID;
-    const JWT_TOKEN = import.meta.env.VITE_SE_JWT_TOKEN;
+    try {
 
-    const url = `https://api.streamelements.com/kappa/v2/sessions/${CHANNEL_ID}`;
+        const response = await fetch('/api/stats');
 
-    const response = await fetch(url, {
-        headers: {
-            'Authorization': `Bearer ${JWT_TOKEN}`,
-            'Accept': 'application/json'
-        }
-    });
+        if (!response.ok) throw new Error('ProxyFailed');
 
-    const data = await response.json();
-    console.log(data);
-    return data;
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
 };
