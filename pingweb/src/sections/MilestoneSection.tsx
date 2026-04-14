4cefb7215663b14ee69bee391324f3105019d040
Ping <pingping7731@gmail.com>
2026-04-15 05:38:28 +0800

    Add : gif webp for milestones,links

--- pingweb/src/sections/MilestoneSection.tsx
+++ pingweb/src/sections/MilestoneSection.tsx
@@ -75,10 +75,22 @@ const MilestoneSection = () => {
 
                             {/* Card Side */}
                             <div className="timeline-card w-full md:w-1/2 flex justify-center md:justify-end z-20">
-                                <GlowCard card={card} index={i}>
-                                    {/*TODO : Make the video play for few sec and let the viewer click to get to the video */}
-                                    <img src={card.imgPath} alt={card.title} className="rounded-lg" />
-                                </GlowCard>
+                                <a
+                                    href={card.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="block cursor-pointer hover:opacity-90 transition-opacity"
+                                >
+                                    <GlowCard card={card} index={i}>
+                                        {/*TODO : Make the video play for few sec and let the viewer click to get to the video */}
+                                        <img
+                                            src={card.imgPath}
+                                            alt={card.title}
+                                            className="rounded-lg"
+                                        />
+
+                                    </GlowCard>
+                                </a>
                             </div>
 
                             {/* Center Dot */}
