237763aa204ea3dcb8d8bc4a085322ca1e789da8
Ping <pingping7731@gmail.com>
2026-04-16 17:06:22 +0800

    grid test

--- pingweb/src/sections/PhotoGrid.tsx
+++ pingweb/src/sections/PhotoGrid.tsx
@@ -1,21 +1,21 @@
 
 // // import { gridPhotos, gridPhotos2, gridPhotos3 } from "../constants";
-// import { gridPhotos } from "../constants";
+import { gridPhotos } from "../constants";
 const PhotoGrid = () => {
     return (
         <div className="carousel pt-16" id="Photos">
-            {/* <div className="innerPart">
+            <div className="innerPart">
                 {gridPhotos.map((photo) => (
                     <div key={photo.name} className="vrcCard hover:scale-110 ease-in-out duration-500 transition-all">
                         <img src={photo.path} alt={photo.name} className="w-full h-full object-cover rounded-xl " />
                     </div>
                 ))}
-                {gridPhotos.map((photo) => (
+                {/* {gridPhotos.map((photo) => (
                     <div key={`${photo.name}-dup`} className="vrcCard hover:scale-110 ease-in-out duration-500 transition-all">
                         <img src={photo.path} alt={photo.name} className="w-full h-full object-cover rounded-xl" />
                     </div>
-                ))}
-            </div> */}
+                ))} */}
+            </div>
             {/* <div className="innerPart2">
                 {gridPhotos2.map((photo) => (
                     <div key={photo.name} className="vrcCard hover:scale-110 ease-in-out duration-500 transition-all">
